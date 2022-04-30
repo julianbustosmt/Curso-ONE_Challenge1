794928859937470e37ba4842c86ff080c30698ad
julianbustosmt <julianestebanbustos@gmail.com>
2022-04-30 11:59:32 -0500

    Funcion de desencriptado

--- js/index.js
+++ js/index.js
@@ -18,12 +18,37 @@ const encrypt = (letter) => {
     return (letter)
 }
 
-const textEncrypt = (text) => {
-    let finalText = ""
-    for(let i = 0; i<text.length; i++){
-        finalText += encrypt(text[i]);
+const encrypttext = (text) => {
+    let encryptedText = ""
+    for (let i = 0; i < text.length; i++) {
+        encryptedText += encrypt(text[i]);
     }
-    return finalText
+    return encryptedText
 }
 
-
+/*Decrypt*/
+const decryptText = (text) => {
+    let decryptedText = ''
+    for (let i = 0; i < text.length;) {
+        switch (text[i]) {
+            case 'e':
+                if (text[i + 4] === 'r') { decryptedText += text[i]; i += 5 }
+                break
+            case 'i':
+                if (text[i + 3] === 's') { decryptedText += text[i]; i += 4 }
+                break
+            case 'a':
+                if (text[i + 1] === 'i') { decryptedText += text[i]; i += 2 }
+                break
+            case 'o':
+                if (text[i + 3] === 'r') { decryptedText += text[i]; i += 4 }
+                break
+            case 'u':
+                if (text[i + 3] === 't') { decryptedText += text[i]; i += 4 }
+                break
+            default:
+                decryptedText += text[i++]
+        }
+    }
+    return decryptedText
+}
